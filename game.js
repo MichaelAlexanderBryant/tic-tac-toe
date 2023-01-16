1fb2721627a7a7d32c5d41c829d0f4e2147a5244
MichaelABryant <m.alexander.bryant@gmail.com>
2023-01-15 19:26:23 -0800

    Add game functionality

--- game.js
+++ game.js
@@ -1,11 +1,11 @@
-const player = (playerName) => {
-    const showName = () => console.log(`${playerName}`)
-    return {showName}
+const marker = (playerMarker) => {
+    const useMarker = () => playerMarker;
+    return {useMarker}
 };
 
-const marker = (playerMarker) => {
-    const {showName} = player(playerName);
-    const useMarker = () => console.log(`${playerMarker}`);
+const player = (playerName, playerMarker) => {
+    const {useMarker} = marker(playerMarker);
+    const showName = () => playerName;
     return {showName, useMarker};
 };
 
@@ -13,14 +13,15 @@ const gameboard = (() => {
     let history = [null, null, null, null, null, null, null, null, null];
     const makePlay = (position, person) => history[position] = person.useMarker();
     const viewBoard = () => history;
-    return {makePlay, viewBoard};
+    const resetBoard = () => history = [null, null, null, null, null, null, null, null, null];
+    return {makePlay, viewBoard, resetBoard};
 })();
 
 function checkForWin(person) {
     let winningCombinations = ["036", "147", "258", "012", "345", "678", "048", "246"];
     for (let i = 0; i < winningCombinations.length; i++) {
         let squaresToCheck = winningCombinations[i].split('');
-        for (let j = 0; i < squaresToCheck.length; j++) {
+        for (let j = 0; j < squaresToCheck.length; j++) {
             if (gameboard.viewBoard()[+squaresToCheck[j]] != person.useMarker()){
                 break;
             };
@@ -40,3 +41,47 @@ function checkForTie() {
         return true;
     };
 };
+
+let squares = document.querySelectorAll('div.square');
+let playerOne = player("Michael", "X");
+let playerTwo = player("Odie", "O");
+let currentPlayer = playerOne;
+
+squares.forEach((square) => { square.addEventListener('click', () => { 
+    let playerSelection = square.id;
+    const squareSelected = document.getElementById(playerSelection);
+    let markerImg = document.createElement('img');
+    let marker = currentPlayer.useMarker();
+    let imgSrc = "./img/" + marker + ".png";
+    markerImg.src = imgSrc;
+    squareSelected.appendChild(markerImg);
+    
+    gameboard.makePlay(+playerSelection, currentPlayer);
+
+    if (checkForWin(currentPlayer)) {
+        alert(currentPlayer.showName() + " WINS!");
+        gameboard.resetBoard();
+        for (let i = 0; i < squares.length; i++) {
+            squares[i].textContent = ''
+        };
+        currentPlayer = playerOne;
+
+    }
+    else if (checkForTie(currentPlayer)) {
+        alert("It's a draw!");
+        gameboard.resetBoard();
+        for (let i = 0; i < squares.length; i++) {
+            squares[i].textContent = ''
+        };
+        currentPlayer = playerOne;
+    }
+    else {
+    if (currentPlayer.useMarker() == 'X') {
+        currentPlayer = playerTwo;
+    }
+    else {
+        currentPlayer = playerOne;
+    };
+};
+
+})});
